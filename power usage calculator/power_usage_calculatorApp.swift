895ee3a8d8150c361ee7a3932b3475507d5e2a14
Brandon [DJ45X] <citylife219@gmail.com>
2026-06-23 16:00:00 -0600

    Initial Commit

---
+++ power usage calculator/power_usage_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  power_usage_calculatorApp.swift
+//  power usage calculator
+//
+//  Created by Brandon on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct power_usage_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
